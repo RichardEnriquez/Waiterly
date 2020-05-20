0223fa944c997d22f4dea1fcfc919154b027184e
hugoskl <hugosklx3@gmail.com>
2020-05-20 16:03:53 +0200

    Aplicando un manager a la aplicacion waiterly

---
+++ app/src/main/java/com/kiwi/waiterly/controladores/WaiterlyManager.java
@@ -0,0 +1,20 @@
+package com.kiwi.waiterly.controladores;
+
+public class WaiterlyManager {
+    private static WaiterlyManager instancia = null;
+    //private Array
+
+    //Constructor
+    private WaiterlyManager(){
+        //damos valor al array de platos para no repetir
+    }
+
+    public static WaiterlyManager getInstance(){
+        if (instancia == null){
+            instancia = new WaiterlyManager();
+        }
+        return instancia;
+    }
+
+    //todo:FUNCIONES
+}
