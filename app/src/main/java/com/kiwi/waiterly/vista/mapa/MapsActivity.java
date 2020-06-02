72451e541b321e188e8c2cc8efd7fff492b8692c
hugoskl <hugosklx3@gmail.com>
2020-06-02 12:49:41 +0200

    añadiendo marcadores para Maps

--- app/src/main/java/com/kiwi/waiterly/vista/mapa/MapsActivity.java
+++ app/src/main/java/com/kiwi/waiterly/vista/mapa/MapsActivity.java
@@ -48,15 +48,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng barcelona = new LatLng(41.3887901, 2.1589899);
         LatLng marcador1 = new LatLng(41.379564, 2.167203);
         LatLng marcador2 = new LatLng(41.394327, 2.191843);
-        mMap.addMarker(new MarkerOptions().position(marcador1).title("Marker 1"));
-        mMap.addMarker(new MarkerOptions().position(marcador2).title("Marker 2"));
+        LatLng marcador3 = new LatLng(41.398121, 2.199290);
+        LatLng marcador4 = new LatLng(41.386235, 2.146300);
+        LatLng marcador5 = new LatLng(41.395933, 2.136832);
+        LatLng marcador6 = new LatLng(41.441696, 2.237285);
+        LatLng marcador7 = new LatLng(41.457493, 2.255982);
+        LatLng marcador8 = new LatLng(41.316202, 2.028248);
+        LatLng marcador9 = new LatLng(41.333813, 2.035098);
+        LatLng marcador10 = new LatLng(41.357954, 2.061158);
+
+
+        mMap.addMarker(new MarkerOptions().position(marcador1).title("Los amigos"));
+        mMap.addMarker(new MarkerOptions().position(marcador2).title("El dorado"));
+        mMap.addMarker(new MarkerOptions().position(marcador3).title("Juan y Luca"));
+        mMap.addMarker(new MarkerOptions().position(marcador4).title("Durums"));
+        mMap.addMarker(new MarkerOptions().position(marcador5).title("Restaurante Jose Fina"));
+        mMap.addMarker(new MarkerOptions().position(marcador6).title("Los Hermanos"));
+        mMap.addMarker(new MarkerOptions().position(marcador7).title("Paella para todos"));
+        mMap.addMarker(new MarkerOptions().position(marcador8).title("Señor Pollo"));
+        mMap.addMarker(new MarkerOptions().position(marcador9).title("Restaurante Boliviano"));
+        mMap.addMarker(new MarkerOptions().position(marcador10).title("Laguna Alalay"));
 
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(barcelona,12), 3000,null);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(barcelona,12), 4000,null);
         mMap.setMaxZoomPreference(1000);
     }
 }
-
-
-
-
